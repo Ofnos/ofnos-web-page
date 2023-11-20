accc83a65cf59c7c598b2c79ff769032a6de9bb2
Adrian Lozano Dominguez <adrianlozanod@hotmail.com>
2023-11-20 12:10:27 -0600

    fix: responsive header and allies

--- angular/tailwind.config.js
+++ angular/tailwind.config.js
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: [
     "./src/**/*.{html,ts}",
   ],
   theme: {
     extend: {},
+    screens: {
+      'xs' : '475px',
+      ...defaultTheme.screens,
+    }
   },
   plugins: [],
 }

224307153c78da395a4239376823ade98dac73c6
Adrian Lozano Dominguez <adrianlozanod@hotmail.com>
2023-11-20 12:26:54 -0600

    feat: fade in animation

--- angular/src/app/views/public/home/home.component.ts
+++ angular/src/app/views/public/home/home.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
 
+  @ViewChild('hero', { read: ElementRef }) heroElement: ElementRef | undefined;
+
+  ngAfterViewInit(): void {
+    window.scroll(0, 1)
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  showElements() {
+    const reveals = document.querySelectorAll('.fade-in');
+    for (let i = 0; i < reveals.length; i++) {
+      const windowHeight = window.innerHeight;
+      const elementTop = reveals[i].getBoundingClientRect().top;
+      const elementVisible = 150;
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add('active');
+      }
+    }
+  }
 }

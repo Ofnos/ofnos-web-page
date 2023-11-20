d47c9487a1705f818083de42b4ceb6e91a9c0b8e
Adrian Lozano Dominguez <adrianlozanod@hotmail.com>
2023-11-20 10:53:08 -0600

    feat: add allies component

---
+++ angular/src/app/components/home-components/allies/allies.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-allies',
+  templateUrl: './allies.component.html',
+  styleUrls: ['./allies.component.scss']
+})
+export class AlliesComponent {
+
+}
--- angular/src/app/components/home-components/home-components.module.ts
+++ angular/src/app/components/home-components/home-components.module.ts
@@ -2,20 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeroComponent } from './hero/hero.component';
 import { ServicesCarouselComponent } from './services-carousel/services-carousel.component';
+import { AlliesComponent } from './allies/allies.component';
 
 
 
 @NgModule({
   declarations: [
     HeroComponent,
-    ServicesCarouselComponent
+    ServicesCarouselComponent,
+    AlliesComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     HeroComponent,
-    ServicesCarouselComponent
+    ServicesCarouselComponent,
+    AlliesComponent,
   ]
 })
 export class HomeComponentsModule { }
